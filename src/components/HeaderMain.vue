7acd52cb9964ac539b678a5ef3bc220cfb3c805c
SaraG94 <sara.gherbassi.94@gmail.com>
2023-03-13 15:45:52 +0100

    START

---
+++ src/components/HeaderMain.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<script>
+  export default{
+    
+  }
+</script>
+
+<template>
+  
+</template>
+
+<style scoped>
+
+</style>
