660ce56d151066d8669e0829a210d984baa24cca
SaraG94 <sara.gherbassi.94@gmail.com>
2023-03-13 19:23:26 +0100

    FEAT style header

--- src/components/HeaderMain.vue
+++ src/components/HeaderMain.vue
@@ -12,35 +12,89 @@
   <!-- Main header -->
   <header>
     <div class="container">
+      <!-- logo -->
       <div class="logo">
         <a href="#">
           <img src="/avada-marketing-logo-2x-200x37.png" alt="logo">
         </a>
       </div>
+      <!-- menu -->
       <div class="menu-header">
         <a href="#" class="home-menu">
           Home
-          <font-awesome-icon icon="fa-solid fa-angle-down"/>
+          <font-awesome-icon class="arrow" icon="fa-solid fa-angle-down"/>
         </a>
         <a href="#">
           Services
-          <font-awesome-icon icon="fa-solid fa-angle-down"/>
+          <font-awesome-icon class="arrow" icon="fa-solid fa-angle-down"/>
         </a>
         <a href="#" v-for="el in menu" :key="el.id"> 
           {{ el }}
         </a>
       </div>
+      <!-- contact -->
       <div class="contact">
         <div class="phone-contact">
-          <font-awesome-icon icon="fa-solid fa-phone"/>
-          <p>(555)802-1234</p>
+          <p>
+            <font-awesome-icon class="phone-icone" icon="fa-solid fa-phone"/>
+            (555)802-1234
+          </p>
         </div>
-        <button class="quote">Free Quote</button>
+        <button class="quote">
+          <strong>Free Quote</strong>
+        </button>
       </div>
     </div>
   </header>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+@use '../style/partials/variables' as *;
 
+.container{
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 22px 0 32px;
+}
+.menu-header{
+  display: flex;
+  gap: 34px;
+
+  a{
+    padding: 10px 5px;
+  }
+  .arrow{
+    vertical-align: middle;
+    margin-left: 5px;
+  }
+}
+
+.contact{
+  display: flex;
+  gap:24px;
+  align-items: center;
+  
+  .quote{
+    padding: 18px 40px;
+    background-color: $light-orange;
+  }
+}
+
+.home-menu,
+.phone-icone,
+button.quote{
+  color: $orange-color;
+}
+.home-menu{
+  border-bottom: 3px solid currentColor;
+}
+// .home-menu::after{
+//   content: '';
+//   display: block;
+//   height: 3px;
+//   width: 100%;
+//   //margin-top: 5px;
+//   background-color: $orange-color;
+// }
 </style>
