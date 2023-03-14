ebd5abac6b3140e868dc0508055e6033d3ab8777
SaraG94 <sara.gherbassi.94@gmail.com>
2023-03-14 12:47:56 +0100

    FEAT SectionBrands components+ style section

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,15 +1,15 @@
 <script>
     import Presentation from './Presentation.vue';
-    import SectionServices from './SectionServices.vue'
+    import SectionServices from './SectionServices.vue';
+    import SectionBrands from './SectionBrands.vue'
     export default{
         components:{
             Presentation,
-            SectionServices
+            SectionServices,
+            SectionBrands
         },
         data(){
-            return{
-                brandsLogo:['/img/abstract.png','/img/cglobal.png','/img/next.png','/img/hemisferio.png','/img/spaces.png','/img/digitalbox.png']
-            }
+
         }
     }
 </script>
@@ -18,47 +18,12 @@
     <main>
       <Presentation/>
       <SectionServices/>
-      <section class="brand">
-        <div class="container">
-            <div class="brand-area">
-                <div class="row brand__text">
-                    <h1 class="brand__title">Some brands we work with</h1>
-                    <p class="text-descrition">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                </div>
-                <div class="row brand-logo">
-                    <div class="col" v-for="bradImg in brandsLogo" :key="bradImg.id">
-                        <img :src="bradImg" alt="">
-                    </div>
-                </div>
-            </div>
-        </div>
-      </section>
-  
+      <SectionBrands/>
+     
     </main>
 </template>
   
 <style lang="scss" scoped>
 @use '../style/partials/variables' as *;
-
-.brand-area{
-    display: flex;
-    flex-direction: column;
-    gap: 70px;
-}
-.row.brand__text{
-    text-align: center;
-    .brand__title{
-        font-size: 54px;
-    }
-    .text-descrition{
-        font-size: 24px;
-    }
-}
-
-.row.brand-logo{
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-}
 
 </style>
---
+++ src/components/SectionBrands.vue
@@ -0,0 +1,51 @@
+<script>
+    export default{
+        data(){
+            return{
+                brandsLogo:['/img/abstract.png','/img/cglobal.png','/img/next.png','/img/hemisferio.png','/img/spaces.png','/img/digitalbox.png']
+            }
+        }
+    }
+</script>
+
+<template>
+    <section class="brand">
+        <div class="container">
+            <div class="row brand__text">
+                <h1 class="brand__title">Some brands we work with</h1>
+                <p class="text-descrition">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+            <div class="row brand-logo">
+                <div class="col" v-for="bradImg in brandsLogo" :key="bradImg.id">
+                    <img :src="bradImg" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+  
+<style lang="scss" scoped>
+.brand{
+    padding: 118px 0;
+}
+.container{
+    display: flex;
+    flex-direction: column;
+    gap: 70px;
+}
+.row.brand__text{
+    text-align: center;
+    .brand__title{
+        font-size: 54px;
+    }
+    .text-descrition{
+        font-size: 24px;
+    }
+}
+
+.row.brand-logo{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+</style>
