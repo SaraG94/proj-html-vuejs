9bc9a4d3f747c3426206ace191e9949827b80736
SaraG94 <sara.gherbassi.94@gmail.com>
2023-03-14 14:23:06 +0100

    FEAT section team

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -19,7 +19,7 @@
       <Presentation/>
       <SectionServices/>
       <SectionBrands/>
-      
+      <!-- card!!! -->
       <div class="consultant-reiff">
         <div class="container">
             <div class="card">
@@ -38,6 +38,62 @@
             </div>
         </div>
       </div>
+
+      <section class="team">
+        <div class="container">
+            <div class="row title">
+                <h1>Trusted bu Leading Organisations</h1>
+            </div>
+            <div class="row cards-team">
+                <div class="col-4">
+                    <div class="card-member">
+                        <img src="/img/case-studies-1-400x450.jpg" alt="">
+                        <p>
+                            How Spaces attracted five milion visitors by improving the content
+                        </p>
+                        <div class="stat">
+                            <h3>200%</h3>
+                            <p>
+                                Higher revenue from digital
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="card-member">
+                        <img src="/img/case-studies-1-400x450.jpg" alt="">
+                        <p>
+                            How Spaces attracted five milion visitors by improving the content
+                        </p>
+                        <div class="stat">
+                            <h3>200%</h3>
+                            <p>
+                                Higher revenue from digital
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="card-member">
+                        <img src="/img/case-studies-1-400x450.jpg" alt="">
+                        <p>
+                            How Spaces attracted five milion visitors by improving the content
+                        </p>
+                        <div class="stat">
+                            <h3>200%</h3>
+                            <p>
+                                Higher revenue from digital
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row more">
+                <button class="orange">Read more case studies</button>
+            </div>
+        </div>
+
+      </section>
     </main>
 </template>
   
@@ -82,5 +138,50 @@ button.phone{
     padding: 15px 39px;
     color: $orange-color;
     background-color: $light-orange;
+}
+//////section cardteam
+section.team{
+    padding: 100px 0;
+}
+section.team .container{
+    display: flex;
+    flex-direction: column;
+    gap: 70px;
+}
+
+.title,
+.more{
+    text-align: center;
+    
+}
+
+.title h1{
+    font-size: 54px;
+    padding: 18px 0;
+}
+
+.cards-team{
+    display: flex;
+    margin: 0 -20px;
+    .col-4{
+        flex-basis: calc((100% /12)*4);
+        padding: 0 20px;
+    }
+}
+.card-member{
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+}
+
+.stat::before{
+    content: '';
+    display: block;
+    margin-bottom: 30px;
+    border-bottom: 1px dashed #F2F2F2;
+}
+
+.more .orange{
+    padding: 18px 41px;
 }
 </style>
